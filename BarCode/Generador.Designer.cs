9c05b4523c9bbbf38fbd513a93f42575e4a6fb8f
Elias Barrera <elias130110@gmail.com>
2016-10-08 23:02:27 -0600

    First Commit

---
+++ BarCode/Generador.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace BarCode
+{
+    partial class Generador
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Generador));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_code = new System.Windows.Forms.TextBox();
+            this.btn_generar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.pantalla = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto a Codificar:";
+            // 
+            // txt_code
+            // 
+            this.txt_code.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_code.Location = new System.Drawing.Point(161, 198);
+            this.txt_code.Name = "txt_code";
+            this.txt_code.Size = new System.Drawing.Size(122, 29);
+            this.txt_code.TabIndex = 1;
+            // 
+            // btn_generar
+            // 
+            this.btn_generar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_generar.Location = new System.Drawing.Point(305, 198);
+            this.btn_generar.Name = "btn_generar";
+            this.btn_generar.Size = new System.Drawing.Size(86, 29);
+            this.btn_generar.TabIndex = 2;
+            this.btn_generar.Text = "Generar";
+            this.btn_generar.UseVisualStyleBackColor = true;
+            this.btn_generar.Click += new System.EventHandler(this.btn_generar_Click);
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Enabled = false;
+            this.btn_guardar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_guardar.Location = new System.Drawing.Point(397, 197);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(82, 31);
+            this.btn_guardar.TabIndex = 3;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // pantalla
+            // 
+            this.pantalla.Location = new System.Drawing.Point(45, 21);
+            this.pantalla.Name = "pantalla";
+            this.pantalla.Size = new System.Drawing.Size(400, 150);
+            this.pantalla.TabIndex = 4;
+            // 
+            // Generador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 256);
+            this.Controls.Add(this.pantalla);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.btn_generar);
+            this.Controls.Add(this.txt_code);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Generador";
+            this.Text = "Generador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_code;
+        private System.Windows.Forms.Button btn_generar;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Panel pantalla;
+    }
+}
+
---
+++ BarCode/Generador.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BarcodeLib;
+using System.Drawing.Imaging;
+
+namespace BarCode
+{
+    public partial class Generador : Form
+    {
+        public Generador()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_generar_Click(object sender, EventArgs e)
+        {
+
+            if (!String.IsNullOrEmpty(txt_code.Text))
+            {
+                Barcode codigo = new Barcode();
+                codigo.IncludeLabel = true;       
+                pantalla.BackgroundImage = codigo.Encode(BarcodeLib.TYPE.CODE128, txt_code.Text, Color.Black, Color.White, 400, 150);
+                btn_guardar.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Debe ingresar un texto a codificar");
+            }
+            
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            Image img_guardada = (Image)pantalla.BackgroundImage.Clone();
+
+            SaveFileDialog SaveDialogo = new SaveFileDialog();
+            SaveDialogo.AddExtension = true;
+            SaveDialogo.Filter = "Image PNG (*.png)|*.png";
+            SaveDialogo.FileName = txt_code.Text;
+            SaveDialogo.ShowDialog();
+
+            if (!String.IsNullOrEmpty(SaveDialogo.FileName))
+            {
+                img_guardada.Save(SaveDialogo.FileName, ImageFormat.Png);
+                txt_code.Text = "";
+                img_guardada.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Debe ingresar un nombre para la imagen");
+
+            }
+            
+
+
+        }
+    }
+}
